d730a003b39fdab3d8338a9d343b2f7309d09508
moeka88 <moekatada8@gmail.com>
2024-07-03 15:25:46 +0900

    ログイン前とログイン後の修正

--- src/Toolbar.js
+++ src/Toolbar.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import './Toolbar.css'; // スタイルシートの読み込み
 
 const Toolbar = () => {
+  let navigate = useNavigate();
+  function goToLogin() {
+    navigate("/login")
+  } 
+  function goToReserve() {
+    navigate("/")
+  } 
+  const debugFlag = false;
+  if(debugFlag){
+    return (
+      <div className="fixed-bar">
+        <button className='reserve-button' onClick={goToReserve}>今すぐ予約する！</button>
+      </div>
+      );
+  }else{
   return (
     <div className="fixed-bar">
-        <button className='reserve-button'>今すぐ予約する！</button>
+      <button className='reserve-button' onClick={goToLogin}>今すぐ予約する！</button>
     </div>
-  );
+    );
+  }
 };
 
 export default Toolbar;
