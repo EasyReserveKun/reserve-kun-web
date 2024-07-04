e28be6b252beaa101631c6158cb425f773f48cc2
noa <snoachi@yahoo.co.jp>
2024-07-04 09:32:17 +0900

    デバッグ用のログアウト関数をデプロイのためコメントアウト

--- src/Home.js
+++ src/Home.js
@@ -24,9 +24,9 @@ function Home() {
   function goToLogin(){
     navigate("/login")
   }
-  function goToLogout(){
-    console.log("log outボタンが押されました。")
-  };
+  // function goToLogout(){
+  //   console.log("log outボタンが押されました。")
+  // };
 
 
   if(sessionStorage.getItem('AccountName') !== null){
