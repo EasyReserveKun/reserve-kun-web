a7a59a68ca9ba11e0358eb3500641e5e8132a8b5
honma <kazuyuki_3735@yahoo.co.jp>
2024-07-04 15:58:47 +0900

    データの最大・最小値設定、時刻ボタンの編集

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,26 @@
 import './Search.css';
 import Modal from './Modal.js'
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 
 function Search() {
   const [show, setShow] = useState(false)
   const [category, setCategory] = useState('')
   const [date, setDate] = useState('')
+  const [tomorrow, setTomorrow] = useState('');
+  const [maxDate, setMaxDate] = useState('');
+
+
+  useEffect(() => {
+    const today = new Date();
+    const tomorrowDate = new Date(today);
+    tomorrowDate.setDate(tomorrowDate.getDate() + 1);
+    const tomorrowISO = tomorrowDate.toISOString().split('T')[0];
+    setTomorrow(tomorrowISO); 
+    
+    const twoMonth = new Date(today.getFullYear(), today.getMonth() + 2, today.getDate());
+    const maxDateISO = twoMonth.toISOString().split('T')[0];
+    setMaxDate(maxDateISO);
+  }, []);
 
   const handleDateChange = (event) => { setDate(event.target.value); };
   const handleCategoryChange = (event) => { setCategory(event.target.value); };
@@ -22,7 +37,8 @@ function Search() {
           <h2>予約登録</h2>
           <div className="row">
             <div className="col-md-4">
-              <input type="date" id="dateInput" className="form-control date-input" placeholder="日付を選択" value={date} onChange={handleDateChange} />
+              <input type="date" id="dateInput" className="form-control date-input" placeholder="日付を選択" 
+              value={date} onChange={handleDateChange} min={tomorrow} max={maxDate} />
             </div>
             <div className="col-md-4">
               <select id="categorySelect" className="form-control category-select" onChange={handleCategoryChange}>
