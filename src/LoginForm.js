8c04e82dd0179e24a97d7205eb521b0dc812ff8a
noa <snoachi@yahoo.co.jp>
2024-07-05 15:44:36 +0900

    新規登録の実装

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -5,24 +5,25 @@ import './LoginForm.css';
 import { useNavigate } from 'react-router-dom'
 
 const LoginForm = () => {
+  const navigate = useNavigate('');
   const [inputCid, setInputCid] = useState('');
   const [inputPassword, setInputPassword] = useState('')
   const [loginError, setLoginError] = useState('')
-  const navigate = useNavigate()
 
   const handleChangeCid = (event) => { setInputCid(event.target.value); }
   const handleChangePassword = (event) => { setInputPassword(event.target.value) }
 
   const loginAuth = async (event) => {
-    if(!inputCid.trim()){
+    event.preventDefault();
+    setLoginError("");
+    if (!inputCid.trim()) {
       await setLoginError("IDを入力してください");
       return null;
     }
-    if(!inputPassword.trim()){
+    if (!inputPassword.trim()) {
       await setLoginError("パスワードを入力してください");
       return null;
     }
-    event.preventDefault();
     let requestData = {
       method: 'POST',
       headers: {
@@ -32,36 +33,37 @@ const LoginForm = () => {
     }
     const responce = await fetch("http://localhost:8080/customer/login", requestData);
     const data = await responce.json();
-  
-    if(data.Name === undefined){
-      setLoginError("アカウントが見つかりません")
-    }else{
-      await sessionStorage.setItem('AccountName',data.Name);
-      await sessionStorage.setItem('AccountMail',data.Mail);
+
+    if (data.status === "Success") {
+      await sessionStorage.setItem('AccountName', data.results.name);
+      await sessionStorage.setItem('AccountMail', data.results.mail);
       setLoginError("")
       navigate("/")
+    } else {
+      setLoginError("アカウントが見つかりません")
+
     }
   }
 
   return (
     <>
-    <Header />
-    <div className="login-container">
-      <h2 className="login-title">ログイン</h2>
-      <form className="login-form">
-        <label htmlFor="email">ID(メールアドレス)</label>
-        <input type="email" id="email" value={inputCid} onChange={handleChangeCid} required />
-        
-        <label htmlFor="password">パスワード</label>
-        <input type="password" id="password" value={inputPassword} onChange={handleChangePassword} required />
-        <p style={{ color: 'red' }}>{loginError}</p>
-        <button type="button" onClick={loginAuth}>ログイン</button>
-      </form>
-      <div className="signup-link">
-        <a href="/signup">アカウントをお持ちでない方はこちら</a>
+      <Header />
+      <div className="login-container">
+        <h2 className="login-title">ログイン</h2>
+        <form className="login-form">
+          <label htmlFor="email">ID(メールアドレス)</label>
+          <input type="email" id="email" value={inputCid} onChange={handleChangeCid} required />
+
+          <label htmlFor="password">パスワード</label>
+          <input type="password" id="password" value={inputPassword} onChange={handleChangePassword} required />
+          <p style={{ color: 'red' }}>{loginError}</p>
+          <button type="button" onClick={loginAuth}>ログイン</button>
+        </form>
+        <div className="signup-link">
+          <a href="/signup">アカウントをお持ちでない方はこちら</a>
+        </div>
       </div>
-    </div>
-    <Footer />
+      <Footer />
     </>
   );
 };
