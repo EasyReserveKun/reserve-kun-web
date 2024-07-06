eb734ac508f0a2d9ff67658d922c42129151a828
BuildTools <unconfigured@null.spigotmc.org>
2024-07-07 00:18:13 +0900

    404時のページ遷移実装

--- src/logout/LogoutSuccess.js
+++ src/logout/LogoutSuccess.js
@@ -25,10 +25,10 @@ function LogoutSuccess() {
     <div className="nf-container">
       <Header className="header" />
       <div className="message">
-      <h3>ログアウトしました</h3>
-            <p>ご利用ありがとうございました。3秒後にホーム画面に戻ります。</p>
-            <p>お急ぎの場合は、下のボタンをクリックしてください。</p>
-            <a href="/" className="home-button">ホームに戻る</a>
+        <h3>ログアウトしました</h3>
+        <p>ご利用ありがとうございました。3秒後にホーム画面に戻ります。</p>
+        <p>お急ぎの場合は、下のボタンをクリックしてください。</p>
+        <a href="/" className="home-button">ホームに戻る</a>
       </div>
       <Footer className="footer" />
     </div>
--- src/nf/Nf.js
+++ src/nf/Nf.js
@@ -1,5 +1,6 @@
 // Import Modules
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 // Import StyleSheets
 import '../App.css';
@@ -10,12 +11,21 @@ import Header from '../common/Header';
 import Footer from '../common/Footer';
 
 function Nf() {
+  const navigate = useNavigate('');
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/');
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
   return (
     <div className="nf-container">
       <Header />
       <div className="message">
         申し訳ございませんが、お探しのページは見つかりませんでした。<br />
-        以下のリンクを使用してホームページに移動してください。<br />
+        3秒後に自動でホーム画面へ移動します。<br />
         <a href="/">ホーム画面へ</a>
       </div>
       <Footer />
