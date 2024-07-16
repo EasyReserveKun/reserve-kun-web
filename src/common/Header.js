d38139dea621aed96f946cc6ffbaa93ed4cfe5e2
Suzuki20010319 <yota.s130319@icloud.com>
2024-07-16 14:28:19 +0900

    削除の実装

--- src/common/Header.js
+++ src/common/Header.js
@@ -48,7 +48,7 @@ const Header = () => {
               <ul>
                 <li><a href="/">ホーム画面</a></li>
                 <li><a href="#category">カテゴリ</a></li>
-                <li><a href="#reserve-info">予約確認/変更/キャンセル</a></li>
+                <li><a href="/ReserveCheck">予約確認/変更/キャンセル</a></li>
                 <li><a href="/Faq">FAQガイドライン <br></br>（よくある質問）</a></li>
                 <li><a href="#form">問い合わせフォーム</a></li>
                 <li><a href="/Access">アクセス</a></li>
