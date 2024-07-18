88c319751134731e0f931913cb4f7e84b0e9281f
moeka88 <moekatada8@gmail.com>
2024-07-18 12:30:59 +0900

    Bootstrapを使用

--- src/AdmHeader.js
+++ src/AdmHeader.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import './AdmHeader.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const AdmHeader = () => {
     let navigate = useNavigate();
+
     return (
-        <header className="admsite-header">
-            <div className="wrapper admsite-header__wrapper">
-                <div className="admlogo">
-                    <img src={`${process.env.PUBLIC_URL}/image/ace-logo.png`} alt="Logo" />
-                </div>
-                <nav className="admnav">
-                    <button className="admnav__toggle" aria-expanded="false" type="button">
-                        menu
-                    </button>
-                    <ul className="admnav__wrapper">
-                        <li className="admnav__item"><a href="#">Home</a></li>
-                        <li className="admnav__item"><a href="#">予約停止(選択)</a></li>
-                        <li className="admnav__item"><a href="#">予約一時停止</a></li>
-                        <li className="admnav__item"><a href="#">予約一覧</a></li>
-                        <li className="admnav__item"><a href="#" className="admbutton-link">Logout</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
+        <Navbar bg="dark" variant="dark" expand="md">
+             <div className="admlogo">
+          <img src={`${process.env.PUBLIC_URL}/image/ace-logo.png`} alt="Logo" />
+        </div>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                    <Nav.Link href="#">Home</Nav.Link>
+                    <Nav.Link href="#">予約停止(選択)</Nav.Link>
+                    <Nav.Link href="#">予約一時停止</Nav.Link>
+                    <Nav.Link href="#">予約一覧</Nav.Link>
+                    <Nav.Link className="admbutton-link" href="#">Logout</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
 };
 
