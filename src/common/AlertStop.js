3ffe19ff748bea3566fa2593f8847f6f0751637a
Suzuki20010319 <yota.s130319@icloud.com>
2024-07-18 12:31:22 +0900

    停止機能完成

---
+++ src/common/AlertStop.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import './LogoutConfirm.css';
+
+const Warn = (props) => {
+    if (props.showWarn) {
+        return (
+            <div id="overlay">
+                <div id="">
+                    <div className="warn-modal">
+                        <p>{props.text}</p>
+                        <button onClick={() => props.setShowWarn(false)} className="warn-close-button">閉じる</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Warn;
