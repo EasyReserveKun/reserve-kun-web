9686d6622ed750c56bdb961bf58cef94ccd3734b
moeka88 <moekatada8@gmail.com>
2024-07-19 10:20:22 +0900

    従業員Home完成

--- src/EmployeePortalHome.js
+++ src/EmployeePortalHome.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './EmployeePortalHome.css'; // スタイルシートをインポートする
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCalendarAlt, faBan, faList } from '@fortawesome/free-solid-svg-icons'; // 使用するアイコンをインポートする
+import { faCalendarAlt, faBan, faList, faUnlockAlt } from '@fortawesome/free-solid-svg-icons'; // 使用するアイコンをインポートする
 import AdmHeader from './AdmHeader'; // ヘッダーコンポーネントをインポートする
 
 const EmployeePortalHome = () => {
@@ -10,43 +10,59 @@ const EmployeePortalHome = () => {
       <>
         <AdmHeader />
         <div className="employee-portal-home">
-          {/* メインコンテンツ */}
-          <main className="main-content">
+          <main className="admmain-content">
             <section className="welcome-section">
-              <h1>ようこそ、<br></br>Ace 従業員ポータルへ</h1>
+              <h1>ようこそ、<br />Ace 従業員ポータルへ</h1>
               <p>ここでは便利なツールや社内ニュースをご利用いただけます。</p>
             </section>
 
             <section className="tools-section">
               <h2>便利なツール</h2>
               <div className="tools-list">
-                <a href="AdminPage" className="tool-item">
-                  <div className="icon">
-                    <FontAwesomeIcon icon={faCalendarAlt} size="4x" />
-                  </div>
-                  <div className="tool-info">
-                    <h3>予約停止 <br></br>(日時選択)</h3>
-                    <p>予約を日時を選択して停止する機能です。</p>
-                  </div>
-                </a>
-                <a href="#stop-reservation-now" className="tool-item">
-                  <div className="icon">
-                    <FontAwesomeIcon icon={faBan} size="4x" />
-                  </div>
-                  <div className="tool-info">
-                    <h3>予約即時停止</h3>
-                    <p>予約を即時停止する機能です。</p>
-                  </div>
-                </a>
-                <a href="ReservationList" className="tool-item">
-                  <div className="icon">
-                    <FontAwesomeIcon icon={faList} size="4x" />
-                  </div>
-                  <div className="tool-info">
-                    <h3>予約一覧表示</h3>
-                    <p>予約の一覧を見れる機能です。</p>
-                  </div>
-                </a>
+                <div className="tool-item">
+                  <a href="AdminPage">
+                    <div className="icon">
+                      <FontAwesomeIcon icon={faCalendarAlt} size="4x" />
+                    </div>
+                    <div className="tool-info">
+                      <h3>予約停止 <br />(日時選択)</h3>
+                      <p>日時選択で予約を停止。</p>
+                    </div>
+                  </a>
+                </div>
+                <div className="tool-item">
+                  <a href="#stop-reservation-now">
+                    <div className="icon">
+                      <FontAwesomeIcon icon={faBan} size="4x" />
+                    </div>
+                    <div className="tool-info">
+                      <h3>予約即時停止</h3>
+                      <p>予約の受付を即時停止。</p>
+                    </div>
+                  </a>
+                </div>
+                <div className="tool-item">
+                  <a href="ReservationList">
+                    <div className="icon">
+                      <FontAwesomeIcon icon={faList} size="4x" />
+                    </div>
+                    <div className="tool-info">
+                      <h3>予約一覧表示</h3>
+                      <p>予約の一覧を見る。</p>
+                    </div>
+                  </a>
+                </div>
+                <div className="tool-item">
+                  <a href="#release-reservation">
+                    <div className="icon">
+                      <FontAwesomeIcon icon={faUnlockAlt} size="4x" />
+                    </div>
+                    <div className="tool-info">
+                      <h3>予約停止解除</h3>
+                      <p>予約停止を解除する。</p>
+                    </div>
+                  </a>
+                </div>
               </div>
             </section>
 
